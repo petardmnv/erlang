d109ec4ef91ad1e27b2b157c31b2d95d525b0985
petardmnv <petar.dmnv@gmail.com>
2021-04-19 19:13:44 +0300

    Add some work

--- ChainOfProcesses/chain.erl
+++ ChainOfProcesses/chain.erl
@@ -1,5 +1,5 @@
 -module(chain).
--export ([loop/2, start/3]).
+-export ([loop/2, start/4, restart_count/2]).
 
 
 %% find flush function in erlang documentation
@@ -21,22 +21,49 @@ loop(N, Parent) ->
 			Parent ! {last_child, LastChildPid};
 			
 		{calculate, G} ->
-			M = G * 5,
-			io:format("I am ~p, Calculated M = ~p~n", [self(), M]),
+			M = G + 1,
+			io:format("I am ~p, Calculated M = ~p, going to Parent ~p~n", [self(), M, Parent]),
 			Parent ! {calculate, M};
+			
 		Any -> Any
 	end,
 	loop(N, Parent).
 
-	
-start(NumberChilds, Parent, G) ->
-	FirstChild = spawn(chain, loop, [NumberChilds, Parent]),
-	FirstChild ! create_ child,
+
+
+start(NumberChilds, Parent, G, C) ->
+	FirstChild = spawn(chain, loop, [NumberChilds, self()]),
+	io:format("Start: My Parent: ~p, My pid ~p~n", [Parent, self()]),
+	FirstChild ! create_child,
 	
 	receive
-		{last_child, LastChildPid} ->
-			LastChildPid ! {calculate, G}
+		{last_child, LastChildPid} -> 
+			io:format("Start: My pid: ~p, Last Child Pid: ~p, sent to Parent pid: ~p~n", [self(), LastChildPid, Parent]),
+			LastChildPid ! {calculate, G},
+			restart_count(LastChildPid, C)
 	end.
+
+
+restart_count(_FirstChild, 1) -> 
+	receive
+		{calculate, M} ->
+			N = M + 1,
+			io:format("Origin N =~p, My Pid: ~p~n", [N, self()])
+	end,
+	io:format("My Pid: ~p and I stopped the summing chain.~n", [self()]);
+restart_count(FirstChild, C) ->
+	receive
+		{calculate, M} ->
+			N = M + 1,
+			io:format("Origin N = ~p, My Pid: ~p~n", [N, self()]),
+			FirstChild ! {calculate, N}
+	end,
+	restart_count(FirstChild, C-1).
+
+	
+
+%%_ - _ - _ - _ - _
+%% new function which keeps start function alive that is the logic becouse we cant loop start function
 	
 %% Make calculations endless
 %% Make calculations to be fixed by size like (50 loops)
